c97b5fe6974cf75344e4e00a2c8cdde573febdef
isikesra <isikesra01@gmail.com>
2024-12-25 20:28:01 +0300

    gunsonu

---
+++ src/day04_MatematikselIslemlerConcatenationsMantıksalIslemler/C01_MatematikselIslemler.java
@@ -0,0 +1,29 @@
+package day04_MatematikselIslemlerConcatenationsMantıksalIslemler;
+
+public class C01_MatematikselIslemler {
+
+    public static void main(String[] args) {
+
+        int a = 14 ;
+        int b= 36 ;
+
+        int c = a+b ; //toplama
+        int d = b-a ; //cıkarma
+        int e = b/a ; //bölme
+        int f = a*b ; //çarpma
+        int g=b%a; //modulus
+        int h=a+d-b*g-e/a;
+
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+        System.out.println("f = " + f);
+        System.out.println("g = " + g);
+        System.out.println("h = " + h);
+
+
+
+    }
+
+
+}
