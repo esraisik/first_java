8bdb032c0aa8ccfd20d957c5470eaa88b45bd160
isikesra <isikesra01@gmail.com>
2024-12-27 00:03:41 +0300

    first

---
+++ src/day05_ifElseStatemant/C02_BagimsizIfCumleleri.java
@@ -0,0 +1,33 @@
+package day05_ifElseStatemant;
+
+import java.util.Scanner;
+
+public class C02_BagimsizIfCumleleri {
+
+    public static void main(String[] args) {
+
+
+        //soru 3- Kullanicidan bir sayi alin,
+        // sayi 3 ile bolunuyorsa ”Uc ile bolunebilen sayi”,
+        // 5 ile bolunebiliyorsa “Bes ile bolunebilen sayi” yazdirin.
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lütfen bir tamsayı giriniz:");
+
+        int girilenSayi = scanner.nextInt();
+
+        if (girilenSayi % 3 == 0) {
+            System.out.println("Uc ile bolunebilen sayi");
+        }
+
+        if (girilenSayi % 5 == 0) {
+            System.out.println("Bes ile bolunebilen sayi");
+
+
+
+
+        }
+    }
+}
